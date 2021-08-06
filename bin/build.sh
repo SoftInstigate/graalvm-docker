80c86007f29004c85cc981afa1c5ec2d7edfab45
Maurizio Turatti <maurizio@softinstigate.com>
2021-08-06 10:49:00 +0200

    Add --build-arg parameter

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --no-cache . -t softinstigate/graalvm
+docker build --build-arg JAVA_VERSION="21.1.0.r16-grl" --no-cache . -t softinstigate/graalvm 
 docker tag softinstigate/graalvm:latest softinstigate/graalvm:latest
