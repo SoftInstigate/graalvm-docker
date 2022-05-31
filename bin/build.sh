15eb1cb5c61e8c35c2e23b269f3e555973f8c1ba
Maurizio Turatti <maurizio@softinstigate.com>
2022-05-31 12:53:42 +0200

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --build-arg JAVA_VERSION="21.1.0.r16-grl" --no-cache . -t softinstigate/graalvm 
+docker build --build-arg JAVA_VERSION="22.1.0.r17-grl" --no-cache . -t softinstigate/graalvm 
 docker tag softinstigate/graalvm:latest softinstigate/graalvm:latest
